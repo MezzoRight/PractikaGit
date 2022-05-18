0f5fca645fd958bb37aabf443b6ba3e43c9b4241
Student_7 <Student_7@STUD31-7>
2022-05-18 10:36:37 +0300

    Add two container

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int b=777;
+        int c=555;
     }
 }
